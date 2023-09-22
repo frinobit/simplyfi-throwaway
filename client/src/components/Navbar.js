1613260af631a9dee17d67f40ff942cb32ec709c
lohkianfeng <lohkianfeng@gmail.com>
2023-09-22 10:57:26 +0800

    changed path

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header>
       <div className="container">
-        <Link to="/simplyfi_mvp">
+        <Link to="/simplyfi_throwaway">
           <h1>Simplyfi</h1>
         </Link>
       </div>
