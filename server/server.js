7e529af4b5344e1df802d1430082d72e9b3b5fdf
lohkianfeng <lohkianfeng@gmail.com>
2023-09-27 09:13:41 +0800

    added goal

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const financialRoutes = require("./routes/financials");
 const userRoutes = require("./routes/user");
+const financialRoutes = require("./routes/financials");
+const goalRoutes = require("./routes/goals");
 
 // express app
 const app = express();
@@ -17,8 +18,9 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("/api/financials", financialRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/financials", financialRoutes);
+app.use("/api/goals", goalRoutes);
 
 // connect to db
 mongoose
