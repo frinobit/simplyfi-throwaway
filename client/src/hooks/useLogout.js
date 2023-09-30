0c456874f8063d6250666244b8742565efbfae6d
lohkianfeng <lohkianfeng@gmail.com>
2023-09-30 11:30:31 +0800

    clear screen before logout

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,9 +1,11 @@
 import { useAuthContext } from "./useAuthContext";
 import { useFinancialsContext } from "./useFinancialsContext";
+import { usePersonalsContext } from "./usePersonalsContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
   const { dispatch: financialsDispatch } = useFinancialsContext();
+  const { dispatch: personalsDispatch } = usePersonalsContext();
 
   const logout = () => {
     // remove user from storage
@@ -11,7 +13,10 @@ export const useLogout = () => {
 
     // dispatch logout action
     dispatch({ type: "LOGOUT" });
+
+    // clear screen
     financialsDispatch({ type: "SET_FINANCIALS", payload: null });
+    personalsDispatch({ type: "SET_PERSONALS", payload: null });
   };
 
   return { logout };
