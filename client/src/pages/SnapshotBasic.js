612469ce6bf5541c34bd877d16dfc6bb5b714a93
lohkianfeng <lohkianfeng@gmail.com>
2023-10-18 20:27:37 +0800

    added refresh button

--- client/src/pages/SnapshotBasic.js
+++ client/src/pages/SnapshotBasic.js
@@ -135,6 +135,20 @@ const SnapshotBasic = () => {
               </div>
             </div>
             <div className={SnapshotCSS.mid_details}>
+              <div className={SnapshotCSS.refresh_details}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#FF7E07"
+                    d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10Zm4.82-4.924A7 7 0 0 0 9.032 5.658l.975 1.755A5 5 0 0 1 17 12h-3l2.82 5.076Zm-1.852 1.266l-.975-1.755A5 5 0 0 1 7 12h3L7.18 6.924a7 7 0 0 0 7.788 11.418Z"
+                  />
+                </svg>
+                <p>Last Refresh: 16/10/23 - 3:14pm</p>
+              </div>
               <div className={SnapshotCSS.couple_image}>
                 <img src="/simplyfi-throwaway/assets/couple.svg" alt="couple" />
               </div>
