1795a964fddf8530b77ad6652dd1f3bb9d3005a2
lohkianfeng <lohkianfeng@gmail.com>
2023-10-25 18:35:07 +0800

    tried adding arrows

--- client/src/pages/utils/financialBox.js
+++ client/src/pages/utils/financialBox.js
@@ -1,10 +1,27 @@
-import SnapshotCSS from "../../styles/pages/SnapshotPremium.module.css";
+import SnapshotCSS from "../../styles/pages/SnapshotBasic.module.css";
 
 export const AssetsBox = ({ description, amount }) => {
   return (
     <div className={`${SnapshotCSS.smallbox} ${SnapshotCSS.greenbox}`}>
       <p>{description}</p>
-      <p>${amount.toLocaleString()}</p>
+      <div className={SnapshotCSS.arrow}>
+        <p>${amount.toLocaleString()}</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M12 20V4m0 0l6 6m-6-6l-6 6"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
@@ -13,7 +30,24 @@ export const LiabilitiesBox = ({ description, amount }) => {
   return (
     <div className={`${SnapshotCSS.smallbox} ${SnapshotCSS.redbox}`}>
       <p>{description}</p>
-      <p>${amount.toLocaleString()}</p>
+      <div className={SnapshotCSS.arrow}>
+        <p>${amount.toLocaleString()}</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M12 4v16m0 0l6-6m-6 6l-6-6"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
