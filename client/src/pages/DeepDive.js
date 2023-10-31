9bbad0e0051e01bc38c10b9dd7b3b84d1ee311ba
lohkianfeng <lohkianfeng@gmail.com>
2023-10-31 22:43:56 +0800

    added frontend

--- client/src/pages/DeepDive.js
+++ client/src/pages/DeepDive.js
@@ -10,13 +10,36 @@ const DeepDive = () => {
   const { user } = useAuthContext();
   const [showSignUp, setShowSignUp] = useState(false);
 
+  const [queryDescription, setQueryDescription] = useState("");
+  //   const [pdf, setPdf] = useState(null);
+
   const handleBackToLogin = () => {
     setShowSignUp(false);
   };
 
+  //   const handleFileChange = (event) => {
+  //     setPdf(event.target.files[0]);
+  //   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted:", queryDescription);
+  };
+
   return (
     <div className={DeepDiveCSS.deepdive}>
-      <h1>hello world</h1>
+      {/* <input type="file" onChange={handleFileChange} /> */}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query"
+          onChange={(e) => setQueryDescription(e.target.value)}
+        />
+        <input type="submit" value="Generate query" />
+      </form>
+
       {!user && !showSignUp && (
         <Login onSignUpClick={() => setShowSignUp(true)} />
       )}
