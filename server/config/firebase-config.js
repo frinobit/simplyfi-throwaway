108d421e6a3cf875cce352c51765520dd7bb42b4
lohkianfeng <lohkianfeng@gmail.com>
2023-11-15 14:38:32 +0800

    added gitignore files

---
+++ server/config/firebase-config.js
@@ -0,0 +1,17 @@
+// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase-admin/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDr3-AlGFMWajEfN3vxMZy8OptvAjUIo0A",
+  authDomain: "testing-simplyask-npfp.firebaseapp.com",
+  projectId: "testing-simplyask-npfp",
+  storageBucket: "testing-simplyask-npfp.appspot.com",
+  messagingSenderId: "611546653745",
+  appId: "1:611546653745:web:5514d8269f1ffc122ce110",
+  // measurementId: "G-CQ04N9T7D6",
+};
+
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
